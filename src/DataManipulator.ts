6282389e12712ab4aa61d7aa2fa15e6ee7419efc
aradhyabob <xviper582@gmail.com>
2023-05-29 19:50:49 +0530

    data manipulator changes

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -11,14 +11,22 @@ export interface Row {
 }
 
 
+
 export class DataManipulator {
-  static generateRow(serverResponds: ServerRespond[]) {
-    return serverResponds.map((el: any) => {
-      return {
-        stock: el.stock,
-        top_ask_price: el.top_ask && el.top_ask.price || 0,
-        timestamp: el.timestamp,
-      };
-    })
+  static generateRow(serverResponds: ServerRespond[]): Row {
+    const priceABC = (serverResponds[0].top_ask_price + serverResponds[0].top_bid_price) / 2;
+    const priceDEF = (serverResponds[1].top_ask_price + serverResponds[1].top_bid_price) / 2;
+    const ratio = priceABC / priceDEF;
+    const upperBound = 1 + 0.10;
+    const lowerBound = 1 + 0.10;
+    return {
+      price_abc : priceABC,
+      price_def : priceDEF,
+      ratio,
+      timestamp: serverResponds[0].timestamp > serverResponds[1].timestamp ?
+        serverResponds[0].timestamp : serverResponds[1].timestamp,
+      upper_bound: upperBound,
+      lower_bound: lowerBound,
+      trigger_alert: (ratio > upperBound || ratio < lowerBound) ? ratio : undefined,
+    };
   }
-}
